736698f3ea392901a6ea0caab17eae137eff3015
Sebastien <delsebastienjames@gmail.com>
2021-05-16 14:54:08 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <p className="title-marvel">
+        <span data-text="M">M</span>
+        <span data-text="A">A</span>
+        <span data-text="R">R</span>
+        <span data-text="V">V</span>
+        <span data-text="E">E</span>
+        <span data-text="L">L</span>
+      </p>
+      <button className="connecter"> se connecter</button>
+      <button className="inscrire"> s'inscrire</button>
+    </div>
+  );
+};
+export default Header;
