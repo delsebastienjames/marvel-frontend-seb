e61c81508c34f4f6c720d1900d101b89368d42f6
Sebastien <delsebastienjames@gmail.com>
2021-05-16 14:54:01 +0200

    Create Characters.js

---
+++ src/components/Characters.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Characters = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/characters");
+        // console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <div>
+      {data.results.map((character, index) => {
+        return (
+          <p className="card">
+            <div className="marvel-comics">
+              <h4 className="name">{character.name}</h4>
+              <img
+                className="picture"
+                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                alt=""
+              />
+              <div className="presentation">
+                {character.description && <div>{character.description}</div>}
+                <br></br>
+                <div className="comics">numéro : {character.comics[0]}</div>
+              </div>
+            </div>
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Characters;
