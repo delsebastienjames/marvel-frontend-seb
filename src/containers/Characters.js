a4be247e5c1ef3ed9fb075f7a57c4f20cf3dc547
Sebastien <delsebastienjames@gmail.com>
2021-05-30 19:46:51 +0200

    Update Characters.js

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom"; // permet de rendre clickable
 
 const Characters = () => {
   const [data, setData] = useState();
@@ -27,21 +28,28 @@ const Characters = () => {
     <div>
       {data.results.map((character, index) => {
         return (
-          <p className="card">
-            <div className="marvel-characters">
-              <h4 className="name">{character.name}</h4>
-              <img
-                className="picture-characters"
-                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                alt=""
-              />
-              <div className="presentation-characters">
-                {character.description && <div>{character.description}</div>}
-                <br></br>
-                {/* <div className="comics">numéro : {character.comics[0]}</div> */}
+          <Link
+            style={{ textDecoration: "none" }}
+            key={character._id}
+            offer={character}
+            to={`/character/${character._id}`}
+          >
+            <p className="card">
+              <div className="marvel-characters">
+                <h4 className="name">{character.name}</h4>
+
+                <img
+                  className="picture-characters"
+                  src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                  alt=""
+                />
+                <div className="presentation-characters">
+                  {character.description && <div>{character.description}</div>}
+                  <br></br>
+                </div>
               </div>
-            </div>
-          </p>
+            </p>
+          </Link>
         );
       })}
       <br></br>
