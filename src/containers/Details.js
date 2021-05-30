64d1693cb7ea4dab2a398415e3f8c0df869f089b
Sebastien <delsebastienjames@gmail.com>
2021-05-30 21:55:43 +0200

    Update Details.js

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -37,10 +37,10 @@ const Details = () => {
             to={`/character/${character._id}`}
           >
             <h4 className="name">{character.name}</h4>
-            <div className="main marvel-characters">
-              <p className="air card">
+            <div className="main ">
+              <p className="card">
                 <img
-                  className="picture-characters"
+                  className="picture"
                   style={{ height: 140 }}
                   // src={character.product_image.secure_url}
                   alt={data.product_name}
