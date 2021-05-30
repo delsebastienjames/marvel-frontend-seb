11cf22fe6c6829ad957b9c88e2f307c35f7431e5
Sebastien <delsebastienjames@gmail.com>
2021-05-30 21:55:45 +0200

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -37,21 +37,22 @@ const Home = () => {
             to={`/character/${character._id}`}
           >
             <h4 className="name">{character.name}</h4>
-            <div className="main marvel-characters">
-              <p className="air card">
+            <div className="main marvel-home">
+              <p className="air-home card">
                 <img
-                  className="picture-characters"
+                  className="picture-home"
                   style={{ height: 140 }}
+                  src={character.product_image}
                   // src={character.product_image.secure_url}
                   alt={data.product_name}
                 />
                 <img
-                  className="picture-characters"
+                  className="picture-home"
                   src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
                   alt=""
                 />
               </p>
-              <div className="presentation-characters">
+              <div className="presentation-home">
                 {character.description && <div>{character.description}</div>}
                 <br></br>
               </div>
