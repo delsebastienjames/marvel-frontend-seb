ca931bcc92ea55287964c3d0983d98778003b0f0
Sebastien <delsebastienjames@gmail.com>
2021-08-17 20:46:05 +0200

    Create Loader.js

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import loader from "../assets/img/loader.svg";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img alt="loader" src={loader} />
+    </div>
+  );
+};
+
+export default Loader;
